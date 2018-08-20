aeb7dba164191f6fb1a219f7a203b4e08f2549b7
Jehoshuah K <musicalbard@gmail.com>
2018-08-20 14:10:35 -0500

    working to finetune dictionary

--- server/services/parser/parser.js
+++ server/services/parser/parser.js
@@ -1,22 +1,23 @@
 const fuzzysearch = require('fuzzysearch')
 
-const { dictionary, commonWords } = require('./dictionary')
+const {dictionary, commonWords} = require('./dictionary')
 
 // const summary = "Hi, I’m Matt! I am a Fullstack Engineer with a keen eye for design and a knack for creativity. I used to be a full time musician but realized that I really loved taking on the daily challenges that coding presents. As a musician, I learned that creating something shouldn’t be hindered by what you have to work with. I embrace the tools at hand and work to design and build something beautiful. I use Node, Express, Postgres, React, React-Native, JavaScript & CSS. (The list continues to grow.) I’m also a songwriter and music theory nerd. (Guitar & pedal steel if your interested.) My main motivation is to build things people love. Gluing together creativity and resourcefulness to make something truly useful."
 
-const summary = "I am a driven, creative, and highly motivated new fullstack developer looking to create unique and innovative applications for the consumer market. I come from a traditional office background where I constantly desired to solve problems by developing dynamic tools using common office software. It was when I was prohibited to use these tools that I realized I should be making solutions full time. I took the step to change my life and discover how much there was to learn during my time in a fullstack immersive program. I see applications as a logical 'next step' in productivity advancement and improvement. That is why intend to bring my experience and creativity to bare in this exciting industry."
+// const summary =
+//   "I am a driven, creative, and highly motivated new fullstack developer looking to create unique and innovative applications for the consumer market. I come from a traditional office background where I constantly desired to solve problems by developing dynamic tools using common office software. It was when I was prohibited to use these tools that I realized I should be making solutions full time. I took the step to change my life and discover how much there was to learn during my time in a fullstack immersive program. I see applications as a logical 'next step' in productivity advancement and improvement. That is why intend to bring my experience and creativity to bare in this exciting industry."
 
+// // const headline = "Fullstack Web Developer with A Keen Eye for Design"
+// const headline = 'Student at Fullstack Academy of Code Chicago'
 
-// const headline = "Fullstack Web Developer with A Keen Eye for Design"
-const headline = "Student at Fullstack Academy of Code Chicago"
-
-const summaryArr = summary.toLowerCase().split(' ').concat(headline.toLowerCase().split(' '))
-
-
+// const summaryArr = summary
+//   .toLowerCase()
+//   .split(' ')
+//   .concat(headline.toLowerCase().split(' '))
 
 // check our dictionary for keyword matches
 const checkDictionary = (summaryArr, obj = {}) => {
-  for (let i=0; i<summaryArr.length; i++) {
+  for (let i = 0; i < summaryArr.length; i++) {
     let word = summaryArr[i]
     if (!commonWords[word]) {
       for (let key in dictionary) {
@@ -28,12 +29,13 @@ const checkDictionary = (summaryArr, obj = {}) => {
       }
     }
   }
+  console.log(obj)
   return obj
 }
 
 // find sort the categories
 // Returns an array of arrays
-const sortCategories = (obj) => {
+const sortCategories = obj => {
   let sorted = []
   for (let key in obj) {
     sorted.push([key, obj[key]])
@@ -48,10 +50,10 @@ const sortCategories = (obj) => {
 // takes an array of sorted arrays
 // returns an array of strings
 // any multiple word string gets a plus add to any whitespace for compatibility with meetup's api
-const getTopCategories = (arr) => {
+const getTopCategories = arr => {
   let numCategories = 3
   let topCategories = []
-  for (let i=0; i<numCategories; i++) {
+  for (let i = 0; i < numCategories; i++) {
     // let regex = /\s*/
     // let word = arr[i][0].replace(regex, '+')
     // console.log(word)
@@ -66,14 +68,16 @@ const getTopCategories = (arr) => {
 }
 
 const parser = (summary, headline) => {
-  const summaryArr = summary.toLowerCase().split(' ').concat(headline.toLowerCase().split(' '))
+  const summaryArr = summary
+    .toLowerCase()
+    .split(' ')
+    .concat(headline.toLowerCase().split(' '))
   let selectedCategories = checkDictionary(summaryArr)
   let sortedCategories = sortCategories(selectedCategories)
   let getCategories = getTopCategories(sortedCategories)
   return getCategories
 }
 
-
-console.log('top', parser(summary, headline))
+// console.log('top', parser(summary, headline))
 
 module.exports = parser
